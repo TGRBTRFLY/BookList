4edb67d0e25fad6b40498f39e005af98f40cf123
PagePetty <pagevpetty@live.com>
2019-04-20 16:04:44 -0600

    finito

--- utils/database.py
+++ utils/database.py
@@ -6,49 +6,59 @@ Concerned wit storing an retrieving books from a database.
 
 
 def create_book_table():
-    with DatabaseConnection() as connection:
+    with DatabaseConnection('data.db') as connection:
         cursor = connection.cursor()
 
         cursor.execute('CREATE TABLE IF NOT EXISTS books(title text, author text, read integer)')
 
 
 def add_book(title, author):
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute('INSERT INTO books VALUES(?, ?, 0)', (title, author))
-
-    connection.commit()
-    connection.close()
+        cursor.execute('INSERT INTO books VALUES(?, ?, 0)', (title, author))
 
 
 def get_all_books():
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute('SELECT * FROM books')
-    books = [{'title': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
-
-    connection.close()
+        cursor.execute('SELECT * FROM books')
+        books = [{'title': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
 
     return books
 
 
 def mark_book_as_read(title):
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute('UPDATE books SET read=1 WHERE title=?', (title,))
-
-    connection.commit()
-    connection.close()
+        cursor.execute('UPDATE books SET read=1 WHERE title=?', (title,))
 
 
 def delete_book(title):
-    connection = sqlite3.connect('data.db')
-    cursor = connection.cursor()
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
 
-    cursor.execute('DELETE FROM books WHERE title=?', (title,))
+        cursor.execute('DELETE FROM books WHERE title=?', (title,))
 
-    connection.commit()
-    connection.close()
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
